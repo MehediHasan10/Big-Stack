333e8ea36b2af62853d8dab82c85a50d8be58972
Mehedi Hasan <MehediHasan10>
2020-09-23 18:37:28 +0600

    23 sep

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require('express');
-const port = process.env.PORT || 3000; 
-// preference to upload the code to hereku or, otherwise to localhost port 3000.
+const mongoose = require('mongoose');
 
 const app = express();
+
+// preference to upload the code to hereku or, otherwise to localhost port 3000.
+const port = process.env.PORT || 3000; 
+
+// mongo config and connection
+const url = require('./setup/myUrl').mongoUrl;
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+}).catch((err) => {
+    console.error(err.message); //Handles initial connection errors
+    process.exit(1); // Exit process with failure
+});
+
+const db = mongoose.connection;
+db.on('error', () => {
+    console.log('> error occurred from the database');
+});
+db.once('open', () => {
+    console.log('> successfully opened the database');
+});
+module.exports = mongoose;
+
+// mongoose
+//     .connect(url)
+//     .then(() => console.log('DB is connected...'))
+//     .catch(err => console.log(`Error: ${err}`));   
+//Database connection
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true });
+// const db = mongoose.connection;
+// db.on("open", () => console.log("DB Connection Successfull !"));
+// db.on("error", (err) => console.log(err));
 
 app.get('/', (req, res) => {
     res.send("Hi there...");
 });
+
+//Bring All routes
+const auth = require('./routes/api/auth');
 
 app.listen(port, () => console.log(`Server is running at port ${port}...`));
--- setup/myUrl.js
+++ setup/myUrl.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoUrl: "mongodb+srv://el06:test1234@meheds.oho65.mongodb.net/bigStack?retryWrites=true&w=majority"
+    mongoUrl: "mongodb+srv://el06:test1234@meheds.oho65.mongodb.net/bigStack?retryWrites=true&w=majority",
+    secret: "mySecret"
 }
